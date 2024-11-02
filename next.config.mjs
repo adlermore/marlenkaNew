98079cd9eb2939b7080b3570114080cffaedbddd
adlermore <armorbita@gmail.com>
2024-11-02 20:45:22 +0400

    Config changes

--- next.config.mjs
+++ next.config.mjs
@@ -6,21 +6,6 @@ const nextConfig = {
           protocol: 'https',
           hostname: '165.232.47.193',
           pathname: '**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'a4.espncdn.com',
-          pathname: '**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'a3.espncdn.com',
-          pathname: '**',
-        },
-        {
-          protocol: 'https',
-          hostname : 'www.usmagazine.com',
-          pathname: '**',
         }
       ]
     }
