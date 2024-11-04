ffdedb822e027432936a589b67d68a1fc9f87b0c
adlermore <armorbita@gmail.com>
2024-11-04 10:35:36 +0400

    Fix: Products Length Bug

--- components/listing/ProductGrid.js
+++ components/listing/ProductGrid.js
@@ -5,13 +5,12 @@ const Product = lazy(() => import('@/components/product/Product'));
 
 const ProductGrid = ({ selectedServings, selectedFlavors, categoryId, searchFilter }) => {
   const [products, setProducts] = useState([]);
-  const initialOffset = 0;
   const [loading, setLoading] = useState(true);
   const [hasMore, setHasMore] = useState(false);
   const [offset, setOffset] = useState(0);
 
   const apiUrl = useMemo(() => {
-    return `${process.env.NEXT_PUBLIC_DATA_API}/getProducts?offset=${offset}&limit=10&filter=${searchFilter}&servings=${selectedServings}&category_id=${categoryId}&flavors=${JSON.stringify(selectedFlavors)}`;
+    return `${process.env.NEXT_PUBLIC_DATA_API}/getProducts?offset=${offset}&limit=9&filter=${searchFilter}&servings=${selectedServings}&category_id=${categoryId}&flavors=${JSON.stringify(selectedFlavors)}`;
   }, [offset, selectedFlavors, categoryId, selectedServings, searchFilter]);
 
   const fetchProducts = useCallback(async () => {
@@ -21,7 +20,7 @@ const ProductGrid = ({ selectedServings, selectedFlavors, categoryId, searchFilt
       if (!response.ok) throw new Error('Network response was not ok');
       const data = await response.json();
       setProducts(prev => (offset === 0 ? data.data.products : [...prev, ...data.data.products]));
-      setHasMore(data.data.products.length === 10);
+      setHasMore(data.data.products.length === 9);
     } catch (error) {
       console.error("Failed to fetch products:", error);
     } finally {
@@ -29,22 +28,22 @@ const ProductGrid = ({ selectedServings, selectedFlavors, categoryId, searchFilt
     }
   }, [apiUrl, offset]);
 
+  // Fetch products when offset changes
   useEffect(() => {
-    setProducts([]);
-    setOffset(initialOffset);
     fetchProducts();
-  }, [fetchProducts, initialOffset]);
+  }, [fetchProducts]);
 
+  // Load more products
   const loadMore = useCallback(() => {
     if (!loading && hasMore) {
-      setOffset(prevOffset => prevOffset + 10);
+      setOffset(prevOffset => prevOffset + 9); // Increment offset by 9
     }
   }, [loading, hasMore]);
 
   return (
     <div className='w-full pb-[60px]'>
       <div className='grid grid-cols-3 tablet:grid-cols-2 mobile:block gap-[20px]'>
-        {loading ? (
+        {loading && products.length === 0 ? (
           Array.from({ length: 6 }).map((_, index) => (
             <div key={index} className="card is-loading">
               <div className="image"></div>
@@ -63,8 +62,30 @@ const ProductGrid = ({ selectedServings, selectedFlavors, categoryId, searchFilt
           </div>
         )}
       </div>
-      {hasMore && !loading && (
-        <button className='more_btn' onClick={loadMore}>Load More Products</button>
+      {hasMore  && (
+        <button className='more_btn' onClick={loadMore}>
+          {loading ?
+            <svg
+              aria-hidden="true"
+              role="status"
+              className="absolute inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              ></path>
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="#1C64F2"
+              ></path>
+            </svg>
+            : 'Load More Products'
+          }
+
+        </button>
       )}
     </div>
   );
